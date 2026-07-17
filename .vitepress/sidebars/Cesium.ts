67fdc86566f41fe75fe096e1abba8963953565b8
jxl <jxl@example.com>
2026-07-17 12:40:08 +0800

    refactor

--- .vitepress/sidebars/Cesium.ts
+++ .vitepress/sidebars/Cesium.ts
@@ -2,6 +2,7 @@ export default {
     text: "Cesium",
     items: [
         {text: '第一个Cesium应用', link: '/Cesium/01_first_project.md'},
+        {text: 'Cesium基础配置', link: '/Cesium/02_basic_settings.md'},
 
     ]
 }
