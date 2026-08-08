54214b87960f4c9b976c7fc23a1a29120d680344
jxl <jxl@example.com>
2026-08-08 17:06:34 +0800

    1

--- .vitepress/sidebars/Figma.ts
+++ .vitepress/sidebars/Figma.ts
@@ -3,5 +3,6 @@ export default {
     items: [
         {text: 'Figma界面认识', link: '/Figma/01_appearance_learn.md'},
         {text: '形状绘制工具', link: '/Figma/02_shape_drawing.md'},
+        {text: '形状属性参数', link: '/Figma/03_shape_attribute_parameters.md'},
     ]
 }
