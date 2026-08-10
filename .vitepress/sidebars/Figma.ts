ec934561c02ada53f5409027b660c9f9722f8bfd
jxl <jxl@example.com>
2026-08-10 15:35:05 +0800

    1

--- .vitepress/sidebars/Figma.ts
+++ .vitepress/sidebars/Figma.ts
@@ -5,5 +5,6 @@ export default {
         {text: '形状绘制工具', link: '/Figma/02_shape_drawing.md'},
         {text: '形状属性参数', link: '/Figma/03_shape_attribute_parameters.md'},
         {text: '布尔运算', link: '/Figma/04_boolean_operation.md'},
+        {text: '形状编辑和钢笔工具', link: '/Figma/05_shape_editing_and_pen_tool.md'},
     ]
 }
