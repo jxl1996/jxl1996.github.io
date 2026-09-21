99b33fd7c6ec7d15b336225d5e4ea0f917dec5bd
jxl <jxl@example.com>
2026-09-21 23:30:12 +0800

    1

--- .vitepress/sidebars/Flutter.ts
+++ .vitepress/sidebars/Flutter.ts
@@ -2,6 +2,7 @@ export default {
     text: "Flutter",
     items: [
         {text: '安装配置', link: '/Flutter/001_environment.md'},
+        {text: '解决web跨域问题', link: '/Flutter/002_cors.md'},
 
     ]
 }
